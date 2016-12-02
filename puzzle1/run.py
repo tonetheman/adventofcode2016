08a41ea080c798449977241e11e39f7a15d81e2f
tony <tonetheman@gmail.com>
2016-12-01 22:04:09 -0600

    got second part

--- puzzle1/run.py
+++ puzzle1/run.py
@@ -21,30 +21,37 @@ def solve(s):
 		cdir = cdir%4
 
 		if cdir == 0:
-			y = y + steps
 			for i in range(steps):
-				if positions.has_key((x,y+i)):
-					print "found it", d, x,y+i, abs(x) + abs(y+i)
+				y = y  + 1
+				if positions.has_key((x,y)):
+					print "found it", d, x,y, abs(x) + abs(y)
 				else:
-					print "i am here",d,x,y+i,abs(x)+abs(y+i)
+					positions[(x,y)] = 1
+					print "i am here",d,x,y,abs(x)+abs(y)
 		elif cdir == 1:
-			x = x + steps
 			for i in range(steps):
-				if positions.has_key((x+i,y)):
-					print "found it", d, x+i,y, abs(x+i) + abs(y)
+				x = x + 1
+				if positions.has_key((x,y)):
+					print "found it", d, x,y, abs(x) + abs(y)
 				else:
-					print "i am here",d,x+i,y,abs(x+i)+abs(y)
-
+					positions[(x,y)] = 1
+					print "i am here",d,x,y,abs(x)+abs(y)
 		elif cdir == 2:
-			y = y - steps
+			for i in range(steps):
+				y = y - 1
+				if positions.has_key((x,y)):
+					print "found it",d,x,y,abs(x) + abs(y)
+				else:
+					positions[(x,y)] = 1
+					print "i am here",d,x,y,abs(x)+abs(y)
 		elif cdir == 3:
-			x = x - steps
-
-		if positions.has_key((x,y)):
-			print "found it",d, x,y, abs(x) + abs(y)
-		else:
-			print "i am here",d, x,y, abs(x) + abs(y)
-			positions[(x,y)] = 1
+			for i in range(steps):
+				x = x - 1
+				if positions.has_key((x,y)):
+					print "found it",d,x,y,abs(x) + abs(y)
+				else:
+					positions[(x,y)] = 1
+					print "i am here",d,x,y,abs(x)+abs(y)
 
 	print x,y, abs(x) + abs(y)
 
@@ -53,5 +60,5 @@ def test():
 	solve("R2,R2,R2")
 	solve("R5,L5,R5,R3")
 
-#solve("L2, L5, L5, R5, L2, L4, R1, R1, L4, R2, R1, L1, L4, R1, L4, L4, R5, R3, R1, L1, R1, L5, L1, R5, L4, R2, L5, L3, L3, R3, L3, R4, R4, L2, L5, R1, R2, L2, L1, R3, R4, L193, R3, L5, R45, L1, R4, R79, L5, L5, R5, R1, L4, R3, R3, L4, R185, L5, L3, L1, R5, L2, R1, R3, R2, L3, L4, L2, R2, L3, L2, L2, L3, L5, R3, R4, L5, R1, R2, L2, R4, R3, L4, L3, L1, R3, R2, R1, R1, L3, R4, L5, R2, R1, R3, L3, L2, L2, R2, R1, R2, R3, L3, L3, R4, L4, R4, R4, R4, L3, L1, L2, R5, R2, R2, R2, L4, L3, L4, R4, L5, L4, R2, L4, L4, R4, R1, R5, L2, L4, L5, L3, L2, L4, L4, R3, L3, L4, R1, L2, R3, L2, R1, R2, R5, L4, L2, L1, L3, R2, R3, L2, L1, L5, L2, L1, R4")
-solve("R8,R4,R4,R8")
+solve("L2, L5, L5, R5, L2, L4, R1, R1, L4, R2, R1, L1, L4, R1, L4, L4, R5, R3, R1, L1, R1, L5, L1, R5, L4, R2, L5, L3, L3, R3, L3, R4, R4, L2, L5, R1, R2, L2, L1, R3, R4, L193, R3, L5, R45, L1, R4, R79, L5, L5, R5, R1, L4, R3, R3, L4, R185, L5, L3, L1, R5, L2, R1, R3, R2, L3, L4, L2, R2, L3, L2, L2, L3, L5, R3, R4, L5, R1, R2, L2, R4, R3, L4, L3, L1, R3, R2, R1, R1, L3, R4, L5, R2, R1, R3, L3, L2, L2, R2, R1, R2, R3, L3, L3, R4, L4, R4, R4, R4, L3, L1, L2, R5, R2, R2, R2, L4, L3, L4, R4, L5, L4, R2, L4, L4, R4, R1, R5, L2, L4, L5, L3, L2, L4, L4, R3, L3, L4, R1, L2, R3, L2, R1, R2, R5, L4, L2, L1, L3, R2, R3, L2, L1, L5, L2, L1, R4")
+# solve("R8,R4,R4,R8")
