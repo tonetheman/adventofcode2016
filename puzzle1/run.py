0ac9baf28f31c716ff2e1b3da6e581d0605f1f55
tony <tonetheman@gmail.com>
2016-12-01 21:00:20 -0600

    working on part 2

--- puzzle1/run.py
+++ puzzle1/run.py
@@ -6,6 +6,8 @@ def solve(s):
 	y = 0
 	cdir = 0
 
+	positions = {}
+
 	data = s.split(",")
 	data = map(string.strip,data)
 	for d in data:
@@ -28,11 +30,20 @@ def solve(s):
 			y = y - steps
 		elif cdir == 3:
 			x = x - steps
-		
+
+
+		if positions.has_key((x,y)):
+			print "found it",d, x,y, abs(x) + abs(y)
+		else:
+			print "i am here",d, x,y, abs(x) + abs(y)
+			positions[(x,y)] = 1
 
 	print x,y, abs(x) + abs(y)
 
-	
-solve("R2,L3")
-solve("R2,R2,R2")
-solve("R5,L5,R5,R3")
+def test():
+	solve("R2,L3")
+	solve("R2,R2,R2")
+	solve("R5,L5,R5,R3")
+
+#solve("L2, L5, L5, R5, L2, L4, R1, R1, L4, R2, R1, L1, L4, R1, L4, L4, R5, R3, R1, L1, R1, L5, L1, R5, L4, R2, L5, L3, L3, R3, L3, R4, R4, L2, L5, R1, R2, L2, L1, R3, R4, L193, R3, L5, R45, L1, R4, R79, L5, L5, R5, R1, L4, R3, R3, L4, R185, L5, L3, L1, R5, L2, R1, R3, R2, L3, L4, L2, R2, L3, L2, L2, L3, L5, R3, R4, L5, R1, R2, L2, R4, R3, L4, L3, L1, R3, R2, R1, R1, L3, R4, L5, R2, R1, R3, L3, L2, L2, R2, R1, R2, R3, L3, L3, R4, L4, R4, R4, R4, L3, L1, L2, R5, R2, R2, R2, L4, L3, L4, R4, L5, L4, R2, L4, L4, R4, R1, R5, L2, L4, L5, L3, L2, L4, L4, R3, L3, L4, R1, L2, R3, L2, R1, R2, R5, L4, L2, L1, L3, R2, R3, L2, L1, L5, L2, L1, R4")
+solve("R8,R4,R4,R8")
