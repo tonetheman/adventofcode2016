5101ef4d090dafcd5ceef02259ef0db75df6d40b
tony <tonetheman@gmail.com>
2016-12-02 08:04:28 -0600

    working

--- puzzle2/run.py
+++ puzzle2/run.py
@@ -2,30 +2,27 @@
 
 class Board:
 	def __init__(self):
-		self.board = [1,2,3,4,5,6,7,8,9]
-		self.pos = self._pos(1,1)
-		self.x = 1
-		self.y = 1
-	def _pos(self,x,y):
-		return x*3 + y
+		self.board = [[1,2,3],[4,5,6],[7,8,9]]
+		self.row = 1
+		self.col = 1
 	def __str__(self):
-		return str(self.pos) + " -> " + str(self.x) + ", " + str(self.y)
+		return str(self.row) + ", " + str(self.col) + " --> " + str(self.board[self.row][self.col])
 	def up(self):
-		if self.y == 0:
+		if self.row == 0:
 			return
-		self.y = self.y-1
+		self.row = self.row-1
 	def down(self):
-		if self.y == 2:
+		if self.row == 2:
 			return
-		self.y = self.y +1
+		self.row = self.row +1
 	def left(self):
-		if self.x == 0:
+		if self.col == 0:
 			return
-		self.x = self.x - 1
+		self.col = self.col - 1
 	def right(self):
-		if self.x == 2:
+		if self.col == 2:
 			return
-		self.x = self.x + 1
+		self.col = self.col + 1
 	def read(self,s):
 		for c in s:
 			if c == "U":
@@ -36,11 +33,13 @@ class Board:
 				self.right()
 			elif c == "D":
 				self.down()
-			print "DBG:", c, self.__str__(),self.board[self._pos(self.x,self.y)]
+			print "DBG:", c, self.__str__(),self.board[self.row][self.col]
 
-		return self.board[self._pos(self.x,self.y)]
+		return self.board[self.row][self.col]
 
 b = Board()
+# print "starting", b
+# print b.read("U")
 print b.read("ULL")
 print b.read("RRDDD")
 print b.read("LURDL")
