8443395565337fd8b7ba7983e064908cbb05b01d
tony <tonetheman@gmail.com>
2016-12-04 10:35:51 -0600

    man stupid hard part 2 no hints

---
+++ puzzle4/correct_answer_part2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python run_part2.py  | grep north
+
+
--- puzzle4/run_part2.py
+++ puzzle4/run_part2.py
@@ -1,3 +1,33 @@
+data = open("data.txt","r").readlines()
+import string
+data = map(string.strip,data)
+
+import re
+
+def check_chksum(letters,chk):
+	result = []
+	for k in range(5):
+		max = -1
+		max_index = -1
+		# find the max value from the list
+		for i in range(len(letters)):
+			if letters[i]>max:
+				max_index = i
+				max = letters[i]
+		# find the first position the max happens
+		for i in range(len(letters)):
+			if letters[i] == max:
+				max_index = i
+				break
+		# print "found max and max index first pos", max, max_index, chr(ord('a')+max_index)
+		result.append(chr(ord('a')+max_index))
+		# zero out the max and find the next letter
+		letters[max_index] = 0
+		max = -1
+		max_index = -1
+	# print "result", result,chk
+	res_s = "".join(result)
+	return chk == res_s
 
 def decrypt(d):
 	import re
@@ -22,5 +52,37 @@ def decrypt(d):
 		# print c,val_c,a,aa,aaa,aaaa
 		res.append(aaaa)
 
-	return res
+	return str(sector_id) + " " + "".join(res)
+
+def checkit(d):
+
+	# break it up into room code and chksum
+	P = re.compile("([a-zA-Z-]+)([0-9]+)\[([a-zA-Z0-9]+)\]")
+	m = P.search(d)
+	code = m.group(1)
+	sector_id = m.group(2)
+	chk = m.group(3)
+
+	# replace the - in the code
+	code = code.replace("-","")
+	code = re.sub("[0-9]","",code)
+
+	code = "".join(sorted(code))
+
+	letters = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+	for c in code:
+		pos = ord(c)-ord('a')
+		letters[pos] = letters[pos] + 1
+
+	matches = check_chksum(letters,chk)
+
+	if matches:
+		print decrypt(d)	
+	else:
+		pass
+
+for d in data:
+	checkit(d)	
+
 
