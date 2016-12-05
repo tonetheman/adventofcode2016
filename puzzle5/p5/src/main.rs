dbd0bc33de50ad78197d509b4614809949fbe305
tony <tonetheman@gmail.com>
2016-12-05 08:28:09 -0600

    looking at data types in rust for digest

--- puzzle5/p5/src/main.rs
+++ puzzle5/p5/src/main.rs
@@ -1,3 +1,23 @@
+
+extern crate md5;
+
 fn main() {
     println!("Hello, world!");
+
+    let digest = md5::compute(b"reyedfim797564");
+    println!(" got it {} {} {} {} {}",
+    digest[0],digest[1],digest[2],digest[3],digest[4]);
+
+    if digest[0] == 0 && digest[1] == 0 && digest[2] == 0
+      && digest[3] == 0 && digest[4] == 0 {
+      println!(" got it {} {} {} {} {}",
+      digest[0],digest[1],digest[2],digest[3],digest[4]);
+    }
+
+
+    let digest2 = md5::compute(b"reyedfim938629");
+    println!(" got it {} {} {} {} {}",
+    digest2[0],digest2[1],digest2[2],digest2[3],digest2[4]);
+
+
 }
