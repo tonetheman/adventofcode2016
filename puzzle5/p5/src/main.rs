986092bf9af5e7d7c7a8b999377d0cd13c7c2f94
tony <tonetheman@gmail.com>
2016-12-05 22:43:46 -0600

    somewhat showing something ha

--- puzzle5/p5/src/main.rs
+++ puzzle5/p5/src/main.rs
@@ -4,20 +4,19 @@ extern crate md5;
 fn main() {
     println!("Hello, world!");
 
-    let digest = md5::compute(b"reyedfim797564");
-    println!(" got it {} {} {} {} {}",
-    digest[0],digest[1],digest[2],digest[3],digest[4]);
+    let prefix = "reyedfim";
 
-    if digest[0] == 0 && digest[1] == 0 && digest[2] == 0
-      && digest[3] == 0 && digest[4] == 0 {
-      println!(" got it {} {} {} {} {}",
-      digest[0],digest[1],digest[2],digest[3],digest[4]);
+    for x in 0..1000000 {
+      let s: String = format!("{}{}",prefix,x.to_string());
+      let digest = md5::compute(s.as_bytes());
+
+      if digest[0] == 0x00 && digest[1] == 0x00  &&
+        (digest[2]>>4)==0x00 {
+          println!("possible match {} {} {} {}",
+          x, digest[0], digest[1], digest[2]);
+          println!("{:X} {:X} {:X}", digest[0], digest[1], digest[2]);
+      }
     }
-
-
-    let digest2 = md5::compute(b"reyedfim938629");
-    println!(" got it {} {} {} {} {}",
-    digest2[0],digest2[1],digest2[2],digest2[3],digest2[4]);
 
 
 }
