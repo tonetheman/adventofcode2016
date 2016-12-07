00e6bcff7795e6f99ec8e058b05c5b9f91a4bea7
tony <tonetheman@gmail.com>
2016-12-07 14:42:32 -0600

    not working guessed 117 and it is not right

--- puzzle7/run.py
+++ puzzle7/run.py
@@ -25,6 +25,7 @@ def support(s):
 		# save off just the data from the brackets
 		c.append(tmpd[0])
 
+	# stuff not in brackets
 	for ts in a:
 		td = P.findall(ts)
 		# did not find anything
@@ -33,33 +34,40 @@ def support(s):
 		# eliminate not valid char 1 must be diff from char 2
 		if td[0][0]==td[0][1]:
 			continue
+		res = True
 
+	# stuff that was in the brackets
+	for ts in c:
+		td = P.findall(ts)
+		if len(td) == 0:
+			continue
+		res = False
+
+	"""
 	print "orig",s
 	print "not brackets",a
 	print "brackets",b
 	print "bracket data",c
 	print
+	"""
+	print s,res
 
-	return res 
+	return res
 
-
+"""
 support("abba[mnop]qrst")
 support("abcd[bddb]xyyx")
 support("aaaa[qwer]tyui")
 support("ioxxoj[asdfgh]zxcvbn")
 support("tony[is]really[cool]")
-
 """
+
 data = open("input.txt","r").readlines()
 import string
 data = map(string.strip,data)
 
+count = 0
 for d in data:
-	a = pal_finder(d)
-	if len(a)==0:
-		continue
-	if a[0][0]==a[0][1]:
-		continue
-	print d,a
-
-"""
+	if support(d):
+		count = count + 1
+print count
