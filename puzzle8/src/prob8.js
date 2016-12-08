de86087fe2cc1b7a3fdbc2e5191660911669d4bf
tony <tonetheman@gmail.com>
2016-12-08 08:19:40 -0600

    screen somewhat working

--- puzzle8/src/prob8.js
+++ puzzle8/src/prob8.js
@@ -1,24 +1,24 @@
 var Puzzle8;
 (function (Puzzle8) {
     var Screen = (function () {
-        function Screen(w, h) {
+        function Screen(rows, cols) {
             this.scr = [];
-            this.width = w;
-            this.height = h;
-            for (var i = 0; i < (this.width * this.height); i++) {
+            this.rows = rows;
+            this.cols = cols;
+            for (var i = 0; i < (this.rows * this.cols); i++) {
                 this.scr[i] = 0;
             }
         }
         Screen.prototype.get = function (row, col) {
-            var idx = row * this.width + col;
+            var idx = row * this.cols + col;
             return this.scr[idx];
         };
         Screen.prototype.rect = function (w, h) {
         };
         Screen.prototype.repr = function () {
             var ts = "";
-            for (var i = 0; i < this.width; i++) {
-                for (var j = 0; j < this.height; j++) {
+            for (var i = 0; i < this.cols; i++) {
+                for (var j = 0; j < this.rows; j++) {
                     ts = ts + this.get(j, i) + " ";
                 }
                 ts = ts + "\n";
--- puzzle8/src/prob8.ts
+++ puzzle8/src/prob8.ts
@@ -3,17 +3,17 @@ namespace Puzzle8 {
 
 	export class Screen {
 		scr : number[] =[];
-		width : number;
-		height : number;
-		constructor(w:number, h:number) {
-			this.width = w;
-			this.height = h;
-			for(let i=0;i<(this.width*this.height);i++) {
+		rows : number;
+		cols : number;
+		constructor(rows:number, cols:number) {
+			this.rows = rows;
+			this.cols = cols;
+			for(let i=0;i<(this.rows*this.cols);i++) {
 				this.scr[i] = 0;
 			}
 		}
 		get(row:number,col:number) : number {
-			let idx : number = row*this.width + col;
+			let idx : number = row*this.cols + col;
 			return this.scr[idx];
 		}
 		rect(w:number, h:number) {
@@ -21,8 +21,8 @@ namespace Puzzle8 {
 		}
 		repr() :string {
 			let ts : string = "";
-			for(let i=0;i<this.width;i++) {
-				for(let j=0;j<this.height;j++) {
+			for(let i=0;i<this.cols;i++) {
+				for(let j=0;j<this.rows;j++) {
 					ts = ts + this.get(j,i) + " ";
 				}
 				ts = ts + "\n";
