8c446efe33a0939e2b77f0fb9fb17863cd541c5b
tony <tonetheman@gmail.com>
2016-12-13 08:31:30 -0600

    printing and setup constants something not right in how i am storing or printing maze

--- puzzle13/day13a.py
+++ puzzle13/day13a.py
@@ -7,7 +7,7 @@ def compute(x,y,designer_number):
     res = x*x + 3*x + 2*x*y + y + y*y
     res = res + designer_number
 
-    print "res",bin(res)
+    # print "res",bin(res)
     BIG = 0xfffffffe
 
     def check(var,pos):
@@ -31,28 +31,41 @@ def compute(x,y,designer_number):
 def set_value(x,y,value):
     maze[y][x] = value
 
-def fill_out_maze():
-    for i in range(3):
-        for j in range(3):
-            compute(i,j,10)
-
+# maze in puzzle is at least 31 x 39
+# since that is target
 def setup_maze():
-    for i in range(6+1):
-        tmp = [0,0,0,0,0,0,0]
+    for i in range(42):
+        tmp = []
+        for j in range(42):
+            tmp.append(0)
         maze.append(tmp)
+
 def print_maze():
-    for i in range(6+1):
-        print maze[i]
+    for i in range(40):
+        this_x = maze[i]
+        for j in range(40):
+            print this_x[j],
+        print
+
+
+DESIGNER_NUMBER = 1362
+
+START_X = 1
+START_Y = 1
+TARGET_X = 31
+TARGET_Y = 39
 
 setup_maze()
 print_maze()
 
-for i in range(6+1):
-    for j in range(6+1):
+for i in range(40):
+    for j in range(40):
         x = j
         y = i
-        res = compute(x,y,10)
-        print x,y,res
+        res = compute(x,y,DESIGNER_NUMBER)
+        # print x,y,res
         set_value(x,y,res)
+
+set_value(TARGET_X,TARGET_Y,"T")
 
 print_maze()
