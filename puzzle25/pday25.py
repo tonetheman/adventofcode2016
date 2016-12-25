b1668fd2b245f60c7ae6d6e2308ce2bc527e05b9
tony <tonetheman@gmail.com>
2016-12-25 12:28:11 -0600

    working checked 16 digits of output slow but works in python

--- puzzle25/pday25.py
+++ puzzle25/pday25.py
@@ -1,6 +1,8 @@
 
 import sys
 
+DEBUG = False
+
 data = open("input.txt","r").readlines()
 import string
 data = map(string.strip,data)
@@ -12,6 +14,7 @@ class State:
         self.b = 0
         self.c = 0
         self.d = 0
+        self.output = []
     def cpy(self):
         s = State()
         s.pc = self.pc
@@ -19,6 +22,8 @@ class State:
         s.b = self.b
         s.c = self.c
         s.d = self.d
+        for i in range(len(self.output)):
+            s.output.append(self.output[i])
         return s
     def __repr__(self):
         return "{} {} {} {} {}".format(self.pc,self.a,self.b,self.c,self.d)
@@ -57,9 +62,10 @@ def get_val_from_state(state,register):
         sys.exit(0)
 
 def i_cpy(instr,state):
-    print "i_cpy called"
     data = instr.split()
-    print "src first, dst second", data
+    if DEBUG:
+        print "i_cpy called"
+        print "src first, dst second", data
     src_arg = data[1]
     dst_arg = data[2]
     # should be src then dst
@@ -74,7 +80,8 @@ def i_cpy(instr,state):
     return new_state
 
 def i_inc(instr,state):
-    print "i_inc called"
+    if DEBUG:
+        print "i_inc called"
     data = instr.split()
     src_arg = data[1]
     new_state = state.cpy()
@@ -85,7 +92,8 @@ def i_inc(instr,state):
     return new_state
 
 def i_dec(instr,state):
-    print "i_dec called"
+    if DEBUG:
+        print "i_dec called"
     data = instr.split()
     src_arg = data[1]
     new_state = state.cpy()
@@ -96,10 +104,12 @@ def i_dec(instr,state):
     return new_state
 
 def i_jnz(instr,state):
-    print "i_jnz called"
+    if DEBUG:
+        print "i_jnz called"
     data = instr.split()
     argx = data[1]
     argy = data[2]
+    new_state = state.cpy()
 
     if is_int(argx):
         xval = int(argx)
@@ -111,22 +121,44 @@ def i_jnz(instr,state):
     else:
         yval = int(get_val_from_state(state,argy))
 
-
     if xval != 0:
-        print "XVAL IS", xval, "bumping pc by", yval
+        if DEBUG:
+            print "XVAL IS", xval, "bumping pc by", yval
         new_state.pc = new_state.pc + yval
     else:
-        print "no jump"
+        if DEBUG:
+            print "no jump"
         new_state.pc = new_state.pc+1
 
     return new_state
 
+def i_out(instr,state):
+    if DEBUG:
+        print "i_out called"
+    data = instr.split()
+    arg = data[1]
+
+    outval = None
+    if is_int(arg):
+        outval = int(arg)
+    else:
+        outval = int(get_val_from_state(state,arg))
+
+    new_state = state.cpy()
+    new_state.pc = new_state.pc + 1
+
+    # print outval,
+    # sys.stdout.flush()
+    new_state.output.append(outval)
+
+    return new_state
 
 
 def execute(program,state):
     instr = program[state.pc]
-    print "execute",instr
-    print "state",state
+    if DEBUG:
+        print "execute",instr
+        print "state",state
     if instr.startswith("cpy"):
         new_state = i_cpy(instr,state)
     elif instr.startswith("inc"):
@@ -135,20 +167,43 @@ def execute(program,state):
         new_state = i_dec(instr,state)
     elif instr.startswith("jnz"):
         new_state = i_jnz(instr,state)
+    elif instr.startswith("out"):
+        new_state = i_out(instr,state)
     else:
         print "ERR: instr not known", instr
         import sys
         sys.exit(0)
     return new_state
 
-count  = 0
-state = State()
-while True:
-    print "------------ state->",state
-    new_state = execute(data,state)
-    state = new_state
 
-    print "**********  state ->",state
-    count = count + 1
-    # if count == 9:
-    #    break
+def check_input(check_value):
+    count  = 0
+    state = State()
+    state.a = check_value
+
+    while True:
+        if DEBUG:
+            print "------------ state->",state
+        new_state = execute(data,state)
+        state = new_state
+
+        if DEBUG:
+            print "**********  state ->",state
+        count = count + 1
+        # if count == 9:
+        #    break
+
+        if len(state.output)==16:
+            if DEBUG:
+                print "stopping got 20 outputs"
+            break
+
+    print check_value, state.output
+    return state.output
+
+
+for i in range(190,500):
+    output = check_input(i)
+    if output == [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]:
+        print "GOT IT"
+        break
